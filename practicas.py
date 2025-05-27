81e0350b122f22527c457bea3b5fbc19780ad6ea
raulsanjuan <raul.sanjuan@elektra.com.mx>
2025-05-27 15:58:03 -0600

    test1

--- practicas.py
+++ practicas.py
@@ -1,12 +1,6 @@
-# Escribe un título y algo de texto para la app:
-'''
-# Este es el título del documento
- 
-Este es un _marcador_.
-'''
- 
-df = pd.DataFrame({'col1': [1,2,3]})
-df # <-- Despliega el conjunto de datos
- 
-x = 10
-'x', x # <-- Escribe el carácter ‘x’ y luego el valor de x
+import streamlit as st
+import pandas as pd
+import numpy as np
+
+dataframe = np.random.randn(10, 20)
+st.dataframe(dataframe)
