eb2a3bd3a71d5c0a55843042924f6c09daabcbab
raulsanjuan <raul.sanjuan@elektra.com.mx>
2025-05-27 15:35:42 -0600

    esructura

--- practicas.py
+++ practicas.py
@@ -1,4 +1,12 @@
-
-
-
-print("Hola mundo")
+# Escribe un título y algo de texto para la app:
+'''
+# Este es el título del documento
+ 
+Este es un _marcador_.
+'''
+ 
+df = pd.DataFrame({'col1': [1,2,3]})
+df # <-- Despliega el conjunto de datos
+ 
+x = 10
+'x', x # <-- Escribe el carácter ‘x’ y luego el valor de x
